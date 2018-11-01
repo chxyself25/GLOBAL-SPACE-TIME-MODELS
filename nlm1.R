39a4a23b0fd0977d5454fe11df86908aaea99c1a
chxyself25 <33331467+chxyself25@users.noreply.github.com>
2018-10-31 21:45:28 -0500

    Update nlm1.R

--- nlm1.R
+++ nlm1.R
@@ -5,14 +5,6 @@ R02 <- readRDS("/Users/apple/Desktop/ISU 2018 fall/STAT 606/final_project/data/1
 R03 <- readRDS("/Users/apple/Desktop/ISU 2018 fall/STAT 606/final_project/data/1870_20_2150_R03.rds")
 R04 <- readRDS("/Users/apple/Desktop/ISU 2018 fall/STAT 606/final_project/data/1870_20_2150_R04.rds")
 R05 <- readRDS("/Users/apple/Desktop/ISU 2018 fall/STAT 606/final_project/data/1870_20_2150_R05.rds")
-# R_bar=mean(c(R01,R02,R03,R04,R05))
-# dim(R05)
-# D1=R01-R_bar;D2=R02-R_bar;D3=R03-R_bar;D4=R04-R_bar;D5=R05-R_bar
-# D1=matrix(D1,1,prod(c(dim(D1)[1:3])))
-# D2=matrix(D1,1,prod(c(dim(D2)[1:3])))
-# D3=matrix(D1,1,prod(c(dim(D3)[1:3])))
-# D4=matrix(D1,1,prod(c(dim(D4)[1:3])))
-# D5=matrix(D1,1,prod(c(dim(D5)[1:3])))
 R01=c(R01[20,,])
 R02=c(R02[20,,])
 R03=c(R03[20,,])
@@ -30,10 +22,6 @@ D5=R05-R_bar
 
 # T=15;N=96;M=1;R=5
 D_mat = cbind(D1,D2,D3,D4,D5)
-# loglike<--0.5*T*N*M*(R-1)*log(2*pi)
-#         -0.5*(R-1)*log(det(Sigma(phi=ph_2, alpha=alpha_2, nu=nu_2, psis=psis_2, landfrac=landfrac_2, T.len = 15, N = 96)))
-#         -0.5*T*N*M*log(R)
-#         -0.5*t(D)%*%solve(kronecker(diag(R),Sigma(phi=ph_2, alpha=alpha_2, nu=nu_2, psis=psis_2, landfrac=landfrac_2, T.len = 15, N = 96)))%*%D
 whole_func<-function(pars,landfrac_2=landfrac[20,], T.len = 15, N = 96, D = D_mat){
   phi_2=pars[1]
   alpha_2=pars[2]
@@ -50,23 +38,55 @@ whole_func<-function(pars,landfrac_2=landfrac[20,], T.len = 15, N = 96, D = D_ma
     B <- B + B0
   }
   loglike<- A - 0.5 * B
-  return(loglike)
+  return((-1)*loglike)
 }
-
-
-# try<-Sigma(phi=0.01, alpha=0.1, nu=0.4, psis=c(0.05,0.05), landfrac=landfrac[20,], T.len = 15, N = 96)
-# try2<-solve(kronecker(diag(5),try))
 
 nlmeControl(msTol = 1e-04) # 1e-07 by default
 result<-nlm(whole_func, p=c(0.01, 0.1, 0.4, 0.05, 0.05), print.level = 2, gradtol = 1e-04)
 ## optim does not work better than nlm
 
 
-### test
-Sigma_mat_test <- Sigma(phi=0.01, alpha=0.01, nu=0.01, psis=c(0.05,0.05), 
-                        landfrac=landfrac[20,], T.len = 15, N = 96)
-
-svd_sig <- svd(Sigma_mat_test)
-svd_sig$d
-
-sum( log(svd_sig$d))
+####################################################################
+source(file = "./covfun5.2.R")
+landfrac <- readRDS("./Data/land_fraction_2010-01.rds")
+R01 <- readRDS("./Data/1870_20_2150_R01.rds")
+R02 <- readRDS("./Data/1870_20_2150_R02.rds")
+R03 <- readRDS("./Data/1870_20_2150_R03.rds")
+R04 <- readRDS("./Data/1870_20_2150_R04.rds")
+R05 <- readRDS("./Data/1870_20_2150_R05.rds")
+# estimate multiple bands
+lat.bds <- seq(2, 42, by = 5)
+initials <- list(b1 = c(0.01, 0.1, 1.8, 0.1, 0.1), 
+                 b2 = c(0.01, 0.5, 3.7, 0.2, 0.2),
+                 b3 = c(exp(-3), 0.7, 3.2, 0.1, 0.1),
+                 b4 = c(exp(-2), 0.9, 2.5, 0.2, 0.1),
+                 b5 = c(0.02, 0.1, 0.5, 1e-04, 1e-04),
+                 b6 = c(0.02, 0.6, 1.7, 0.2, 0.1),
+                 b7 = c(exp(-2), 0.6, 2.6, 0.1, 0.1),
+                 b8 = c(exp(-3), 0.3, 2.2, 0.1, 0.1),
+                 b9 = c(exp(-4), 0.2, 1.6, 0.3, 0.3))
+pars.bds <- foreach(i = 1:length(lat.bds), .combine = 'rbind') %dopar% {
+  R1=c(R01[lat.bds[i],,])
+  R2=c(R02[lat.bds[i],,])
+  R3=c(R03[lat.bds[i],,])
+  R4=c(R04[lat.bds[i],,])
+  R5=c(R05[lat.bds[i],,])
+  R <- cbind(R1,R2,R3,R4,R5)
+  R_bar <- apply(R,1,mean)
+  
+  D1=R1-R_bar
+  D2=R2-R_bar
+  D3=R3-R_bar
+  D4=R4-R_bar
+  D5=R5-R_bar
+  
+  D_mat = cbind(D1,D2,D3,D4,D5)
+  nlmeControl(msTol = 1e-04)
+  #result<-nlm(whole_func, p=initials[[paste0("b",i)]], D = D_mat, landfrac = landfrac[lat.bds[i],], 
+              #print.level = 2, gradtol = 1e-04, iterlim = 50)
+  ui.mat <- matrix(0, ncol = 5, nrow = 3) # constrain optim, first three parameters are positive
+  ui.mat[1:3, 1:3] <- diag(1, 3)
+  result <- constrOptim(theta = initials[[paste0("b",i)]], whole_func, D = D_mat, landfrac = landfrac[lat.bds[i],],
+                        grad = NULL, ui = ui.mat, ci = rep(0, 3), outer.eps = 1e-04)
+  result$estimate
+}
